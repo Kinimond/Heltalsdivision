72bb674e7d8439b7d60b3bd10d896580586ed989
Patrick <patrickhass84@gmail.com>
2024-02-13 11:12:24 +0100

    Add project files.

---
+++ Heltalsdivision/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Heltalsdivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            double resultat;
+            int restDel;
+
+            Console.Write("Indtast dit første tal: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Indtast dit andet tal: ");
+            b = int.Parse(Console.ReadLine());
+
+            resultat = a / b;
+            restDel = a % b;
+            Console.WriteLine("Dit resultat er: " + resultat + "\nDin restdel er: " + restDel);
+
+
+
+        }
+    }
+}
